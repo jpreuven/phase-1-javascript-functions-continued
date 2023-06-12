9a37a5cb1c95846837b2e7264107b8a38f435b43
Jonathan <jpreuven@gmail.com>
2023-06-12 14:11:42 -0700

    COMPLETED LESSON

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const saturdayFun = function (want = "roller-skate") {
+  return `This Saturday, I want to ${want}!`;
+};
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+const wrapAdjective = function (flair = "*") {
+  return function (blah = "special") {
+    return `You are ${flair}${blah}${flair}!`;
+  };
+};
